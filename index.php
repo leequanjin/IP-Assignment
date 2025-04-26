16a8e9ac665c84c63b4611e0f77bce674908f741
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2025-04-26 14:59:07 +0800

    testing

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home Page</title>
     </head>
     <body>
         <?php
