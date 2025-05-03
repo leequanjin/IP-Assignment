cf42db48d7e8affafaca990fbb3d5a43c1e30ec0
kee <keekeshen@gmail.com>
2025-05-03 16:26:49 +0800

    user management index

---
+++ files/proxy/AdminProxy.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'AccessInterface.php';
+
+class AdminProxy implements AccessInterface {
+    public function grantAccess() {
+        session_start();
+        if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'staff') {
+            header("Location: ../views/admin_login_view.php");
+            exit();
+        }
+        return true;
+    }
+    public function grantAccess2() {
+        session_start();
+        if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'staff') {
+            header("Location: views/admin_login_view.php");
+            exit();
+        }
+        return true;
+    }
+}
+?>
--- files/usermanagementindex.php
+++ files/usermanagementindex.php
@@ -8,11 +8,11 @@
 
     <p>Please choose how you would like to log in:</p>
 
-    <a href="/views/user_login_view.php">
+    <a href="views/user_login_view.php">
         <button style="padding: 10px 20px;">Login as User</button>
     </a>
 
-    <a href="/views/admin_login_view.php">
+    <a href="views/admin_login_view.php">
         <button style="padding: 10px 20px; margin-left: 10px;">Login as Admin</button>
     </a>
 </body>
