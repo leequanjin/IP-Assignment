977dfc3da5f0bfdcde0b253788233120e83ccecf
kee <keekeshen@gmail.com>
2025-05-03 16:16:13 +0800

    user management index

---
+++ files/usermanagementindex.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Hup Hwa Furniture Store</title>
+</head>
+<body>
+    <h1>Welcome to Hup Hwa Furniture Store</h1>
+
+    <p>Please choose how you would like to log in:</p>
+
+    <a href="/views/user_login_view.php">
+        <button style="padding: 10px 20px;">Login as User</button>
+    </a>
+
+    <a href="/views/admin_login_view.php">
+        <button style="padding: 10px 20px; margin-left: 10px;">Login as Admin</button>
+    </a>
+</body>
+</html>
